4bbf7a8a1b89780ecd73409b5db8754502a698b3
Alex Kehayias <alex.kehayias@gmail.com>
2014-02-03 13:50:15 -0500

    Clean up deps. Update to latest amazonica

--- project.clj
+++ project.clj
@@ -2,10 +2,9 @@
   :description "Archive data to a storage backend"
   :url "http://www.shareablee.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.4.4"]
-                 [org.clojure/tools.nrepl "0.2.2"]
-                 [cronj "0.6.1"]
+                 [clj-time "0.4.4"]                 
                  [cheshire "5.2.0"]
-                 [storm "0.9.0.1"]
-                 [amazonica "0.1.32"]
-                 [org.apache.httpcomponents/httpclient "4.2.2"]])
+                 [amazonica "0.2.3"]
+                 [org.apache.httpcomponents/httpclient "4.2.2"]]
+  :profiles {:dev {:dependencies [[storm "0.9.0.1"]
+                                  [org.clojure/tools.nrepl "0.2.2"]]}})
