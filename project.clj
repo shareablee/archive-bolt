b53a7e8a30b84614881183dbd257187eadbe0916
Jeremy Heiler <jeremyheiler@gmail.com>
2015-03-19 13:59:05 -0400

    AOT compile; release 0.1.9

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.shareablee/archive-bolt "0.1.8"
+(defproject com.shareablee/archive-bolt "0.1.9"
   :description "Reusable storm bolt for storing data to S3"
   :url "https://github.com/shareablee/archive-bolt"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot :all
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.4.4"]                 
                  [cheshire "5.2.0"]
