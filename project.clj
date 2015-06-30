fe4e4192562d7b7bff2456d8f8cf312a8930f6f7
Jeremy Heiler <jeremyheiler@gmail.com>
2015-06-30 15:19:23 -0400

    Update depednencies and add our private jar repo

--- project.clj
+++ project.clj
@@ -4,10 +4,22 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.storm/storm-core "0.9.3"]
-                 [clj-time "0.4.4"]                 
+  :dependencies [[amazonica "0.2.3"]
                  [cheshire "5.2.0"]
-                 [amazonica "0.2.3"]
-                 [org.apache.httpcomponents/httpclient "4.2.5"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.2"]]}})
+                 [clj-time "0.4.4"]]
+  :profiles
+  {:dev
+   {:dependencies [[org.apache.storm/storm-core "0.9.3"]
+                   [org.clojure/clojure "1.5.1"]
+                   [org.clojure/tools.nrepl "0.2.2"]]}}
+  :repositories
+  [["releases"
+    {:url "s3p://shareablee-jar-repo/releases"
+     :username :env/shareablee_aws_access_key
+     :passphrase :env/shareablee_aws_secret_access_key
+     :sign-releases false
+     :snapshots false}]
+   ["snapshots"
+    {:url "s3p://shareablee-jar-repo/snapshots"
+     :username :env/shareablee_aws_access_key
+     :passphrase :env/shareablee_aws_secret_access_key}]])
